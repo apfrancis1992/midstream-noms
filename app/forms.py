19933269911ccdf7fdcf5c6be9f2150c863c8cd5
Alex Francis <afrancis1992@gmail.com>
2021-01-09 09:54:57 -0700

    Ignore

--- app/forms.py
+++ app/forms.py
@@ -66,3 +66,18 @@ class AdminAddUserForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     admin = BooleanField('Admin User')
     submit = SubmitField('Submit')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
