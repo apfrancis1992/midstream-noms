307b6551ca0a0859081ce30870ab045bc6616ca3
Alex Francis <afrancis1992@gmail.com>
2021-01-12 07:56:55 -0700

    Improvements

--- microblog.py
+++
@@ -1,6 +0,0 @@
-from app import app, db
-from app.models import User, Nom, Contract
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Nominations': Nom, 'Contract': Contract}
